602de761b1f8bd20847ba6621b613ef79b35078a
hulko.s <hulko.s@enfins.com>
2018-10-07 16:47:49 +0300

    intermediate commit

--- app/src/main/java/com/example/admin/androidvortchat/MainActivity.java
+++ app/src/main/java/com/example/admin/androidvortchat/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.admin.androidvortchat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.admin.androidvortchat.Factories.HelpFactory;
 import com.example.admin.androidvortchat.Factories.interfaces.FactoryInterface;
@@ -20,18 +24,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         FactoryInterface helpFactory = HelpFactory.innit();
         helpFactory.activate("SocketListener");
+
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        final Button sendButton = findViewById(R.id.button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText input = findViewById(R.id.editText);
+                String text = input.getText().toString();
+                Intent intent = getIntent();
+
             }
         });
+
     }
 
     @Override
